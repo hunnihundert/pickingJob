bed0586ccb5856cd75506c9061d5babfa8baab9f
Hoon Jung <hoon.michael.jung@gmail.com>
2021-02-24 07:11:39 +0900

    Init commit

---
+++ app/src/main/java/com/hooni/pickingjob/ui/view/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.hooni.pickingjob.ui.view
+
+import androidx.fragment.app.Fragment
+
+class LoginFragment : Fragment() {
+
+
+}
---
+++ app/src/main/java/com/hooni/pickingjob/ui/view/PickJobsFragment.kt
@@ -0,0 +1,7 @@
+package com.hooni.pickingjob.ui.view
+
+import androidx.fragment.app.Fragment
+
+class PickJobsFragment: Fragment() {
+
+}
